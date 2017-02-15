a737dd2b04b6e95ec0021aafbc55235ff3d99838
Tolga Balci <tbalci@advance-auto.com>
2017-02-15 17:23:16 -0500

    Initial Setup with Material and Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+var config = {
+  apiKey: "AIzaSyAMLD1D6GQQ3gy9_hcwGa9UUkkAYPSzFl8",
+  authDomain: "learndb-tolga.firebaseapp.com",
+  databaseURL: "https://learndb-tolga.firebaseio.com",
+  storageBucket: "learndb-tolga.appspot.com",
+  messagingSenderId: "692251656873"
+};
+
+const authConfig = {
+  method: AuthMethods.Redirect,
+  provider: AuthProviders.Google
+};
 
 @NgModule({
   declarations: [
@@ -12,6 +27,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(config, authConfig),
     HttpModule
   ],
   providers: [],
