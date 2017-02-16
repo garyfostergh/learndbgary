7f5bdb4221ec938f6b9715981445ebad489f6da4
Tolga Balci <tbalci@advance-auto.com>
2017-02-16 10:17:16 -0500

    Created a default test

--- src/app/firebase-play/firebase-play.component.ts
+++ src/app/firebase-play/firebase-play.component.ts
@@ -10,8 +10,8 @@ export class FirebasePlayComponent implements OnInit {
     allData;
     selectData;
 
-    constructor(private af: AngularFire) { 
-        this.af.database.object("/").remove();        
+    constructor(private af: AngularFire) {
+        this.af.database.object("/").remove();
     }
 
     ngOnInit() {
@@ -19,7 +19,41 @@ export class FirebasePlayComponent implements OnInit {
     }
 
     async test1() {
+        const root = this.af.database.object("/");
+        await root.set(
+            {
+                "items": {
+                    "1002001": {
+                        "description": "MtDew",
+                        "manpartno": "MtDew",
+                        "onHand": 40
+                    },
+                    "1002002": {
+                        "description": "Regular Pepsi",
+                        "manpartno": "Pepsi",
+                        "onHand": 44
+                    },
+                    "1002003": {
+                        "description": "Diet Pepsi",
+                        "manpartno": "PepsiDiet",
+                        "onHand": 100
+                    },
+                    "1002004": {
+                        "description": "Lemon Pepsi",
+                        "manpartno": "PepsiLemon",
+                        "onHand": 3
+                    },
+                    "1002005": {
+                        "description": "Sprite",
+                        "manpartno": "Sprite",
+                        "onHand": 20
+                    }
+                }
+            }
+        );
 
+        this.af.database.object("items/1002001")
+            .subscribe(item => this.selectData = item);
     }
 
     async test2() {
