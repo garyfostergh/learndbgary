9c24704bcb9c976dc8ba23f9f0ed6573011d5e04
Tolga Balci <tbalci@advance-auto.com>
2017-02-16 04:38:47 -0500

    Clean

--- src/app/firebase-play/firebase-play.component.ts
+++ src/app/firebase-play/firebase-play.component.ts
@@ -19,90 +19,22 @@ export class FirebasePlayComponent implements OnInit {
     }
 
     async test1() {
-        const root = this.af.database.object("/");
-        await root.set(
-            {
-                "users": {
-                    "1001": {
-                        "groups": {
-                            "1001": true,
-                            "1002": true,
-                            "1005": true
-                        }
-                    },
-                    "1002": {
-                        "groups": {
-                            "1001": true,
-                            "1009": true
-                        }
-                    },
-                    "1005": {
-                        "groups": {
-                            "1001": true,
-                            "1004": true,
-                            "1011": true,
-                            "1014": true,
-                            "1021": true,
-                            "1024": true,
-                            "1007": true
-                        }
-                    }
-                }
-            }
-        );
 
-        const groups = this.af.database.list("users/1001/groups");    
-        groups
-            .do(console.log)    
-            .subscribe(groups => this.selectData = groups);
     }
-
 
     async test2() {
-        const root = this.af.database.object("/");
-        await root.set(
-{
-    "transactions": {
-        "1001": {
-            "lines": {
-                "1001": {
-                    "item": {
-                        "sku": 1002001,
-                        "description": "MtDew",
-                        "planogram": {
-                            "name": "Drink-a-gon",
-                            "location": "backroom",
-                            "seqnum": 14
-                        }
-                    },
-                    "discounts": {
-                        "1": {
-                            "amount": 1,
-                            "type": "employee"
-                        },
-                        "2": {
-                            "amount": 0.5,
-                            "type": "Buy one get one"
-                        }
-                    }
-                }
-            }
-        }
+
     }
-}
-        );
 
-        await root.update(
-{
-                "transactions/1001/lines/1001/item/sku": "1002002",
-                "transactions/1001/lines/1001/item/description": "MtWho",
-}
-        );
+    async test3() {
 
-        const groups = this.af.database.object("transactions/1001/lines/1001/item/sku");    
-        groups
-            .do(data => console.log("Update received for selectData", data))    
-            .subscribe(groups => this.selectData = groups,
-            err => console.log("Error Retrieving selectData", err));
+    }
+
+    async test4() {
+
+    }
+
+    async test5() {
+
     }
 }
